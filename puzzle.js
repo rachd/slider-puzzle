67d66e01d90f5511fd309916688f29accd899d73
rachd <rachel.m.dorn@gmail.com>
2018-04-26 22:40:40 -0400

    generate moves

--- puzzle.js
+++ puzzle.js
@@ -1,29 +1,133 @@
+class Node {
+    constructor(gameState, moves) {
+        this.gameState = gameState;
+        this.moves = moves;
+    }
+}
+
 const checkPuzzle = (puzzle) => {
     let i = 1;
-    for (let row of puzzle) {
-        for (let col of row) {
-            if (col !== i) {
-                if (i == puzzle.length * puzzle[0].length && col === -1) {
-                    return true;
-                }
-                return false;
+    for (let entry of puzzle) {
+        if (entry !== i) {
+            if (i == puzzle.length && entry === -1) {
+                return true;
             }
-            i++;
+            return false;
         }
+        i++;
     }
     return true;
 }
 
-const tests = [
-    [[1, 2, 3],
-    [4, 5, 6],
-    [7, 8, -1]],
+const rightMove = (puzzle, spaceIndex) => {
+    return puzzle.map((entry, index) => {
+        if (index === spaceIndex) {
+            return puzzle[index + 1];
+        }
+        if (index === spaceIndex + 1) {
+            return -1;
+        }
+        return entry;
+    });
+}
 
-    [[2, 6, 3],
-    [-1, 1, 8],
-    [4, 7, 5]]
+const leftMove = (puzzle, spaceIndex) => {
+    return puzzle.map((entry, index) => {
+        if (index === spaceIndex) {
+            return puzzle[index - 1];
+        }
+        if (index === spaceIndex - 1) {
+            return -1;
+        }
+        return entry;
+    });
+}
+
+const upMove = (puzzle, spaceIndex) => {
+    const puzzleSize = Math.sqrt(puzzle.length);
+    return puzzle.map((entry, index) => {
+        if (index === spaceIndex) {
+            return puzzle[index - puzzleSize];
+        }
+        if (index === spaceIndex - puzzleSize) {
+            return -1;
+        }
+        return entry;
+    });
+}
+
+const downMove = (puzzle, spaceIndex) => {
+    const puzzleSize = Math.sqrt(puzzle.length);
+    return puzzle.map((entry, index) => {
+        if (index === spaceIndex) {
+            return puzzle[index + puzzleSize];
+        }
+        if (index === spaceIndex + puzzleSize) {
+            return -1;
+        }
+        return entry;
+    });
+}
+
+
+const generateMoves = (puzzle) => {
+    const spaceIndex = puzzle.indexOf(-1);
+    switch(spaceIndex) {
+        case 0:
+            return [rightMove(puzzle, spaceIndex),
+                downMove(puzzle, spaceIndex)];
+        case 1:
+            return [rightMove(puzzle, spaceIndex),
+                downMove(puzzle, spaceIndex),
+                leftMove(puzzle, spaceIndex)];
+        case 2:
+            return [downMove(puzzle, spaceIndex),
+                leftMove(puzzle, spaceIndex)];
+        case 3:
+            return [rightMove(puzzle, spaceIndex),
+                downMove(puzzle, spaceIndex),
+                upMove(puzzle, spaceIndex)];
+        case 4: 
+            return [rightMove(puzzle, spaceIndex),
+                downMove(puzzle, spaceIndex),
+                upMove(puzzle, spaceIndex),
+                leftMove(puzzle, spaceIndex)];
+        case 5:
+            return [downMove(puzzle, spaceIndex),
+                upMove(puzzle, spaceIndex),
+                leftMove(puzzle, spaceIndex)];
+        case 6:
+            return [rightMove(puzzle, spaceIndex),
+                upMove(puzzle, spaceIndex)];
+        case 7: 
+            return [rightMove(puzzle, spaceIndex),
+                upMove(puzzle, spaceIndex),
+                leftMove(puzzle, spaceIndex)];
+        case 8:
+            return [upMove(puzzle, spaceIndex),
+                leftMove(puzzle, spaceIndex)];
+        default:
+            return null;
+    }
+    
+}
+
+const tests = [
+    [1, 2, 3,
+    4, 5, 6,
+    7, 8, -1],
+
+    [2, 6, 3,
+    -1, 1, 8,
+    4, 7, 5]
 ];
 
 for (test of tests) {
-    console.log(checkPuzzle(test));
+    let tree = new Node(test);
+    let queue = [];
+
+    for (let child of generateMoves(tree.gameState)) {
+        console.log(child);
+    }
 }
+
